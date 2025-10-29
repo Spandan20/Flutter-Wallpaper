bc7061de5a36e3bd680cd928375afc2048eecf38
Spandan20 <spandanbusiness@outlook.com>
2025-10-29 23:53:12 +0530

    Finished Integrating Pixels Api
    Learned about future builder
    	- Future builder requires you to return a widget so if you are
    	  using different states for different stages of fetching data
    	  make sure to use if-else chaining otherwise the compiler
    	  complains(which is a good thing)
    The fact this worked on first try feels kind of like magic.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,49 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/widgets/image_card.dart';
+import 'package:http/http.dart' as http;
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late Future<List<dynamic>> _futurePhotos;
+
+  Future<List<dynamic>> fetchPhotos() async {
+    const apiKey = String.fromEnvironment("API_KEY");
+    if (apiKey.isEmpty) {
+      throw Exception("API_KEY not found");
+    }
+    const uri = "https://api.pexels.com/v1/search?query=people";
+    try {
+      final response = await http.get(
+        Uri.parse(uri),
+        headers: {
+          "Authorization": apiKey
+        }
+      );
+      if (response.statusCode == 200) {
+        final Map<String,dynamic> data = jsonDecode(response.body);
+        return data['photos'];
+      }
+      else {
+        throw Exception("Error loading photos: ${response.statusCode}");
+      }
+    } catch(e) {
+      throw Exception("Error: $e");
+    }
+  } 
+
+  @override
+  void initState() {
+    super.initState();
+    _futurePhotos = fetchPhotos();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,18 +51,43 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Center(child: Text("Wallpapers"))
       ),
-      body: GridView.builder(
-        itemCount: 1,
-        padding: EdgeInsets.all(20),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 15,
-          mainAxisSpacing: 15,
-          ), 
-        itemBuilder: (context, index) {
-          return ImageCard();
-        },
-        )
+      body: FutureBuilder<List<dynamic>>(
+        future: _futurePhotos,
+        builder: (context, asyncSnapshot) {
+          if (asyncSnapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          else if (asyncSnapshot.hasError) {
+            return Center(child: Text("Error: ${asyncSnapshot.error}"));
+          }
+
+          else if (asyncSnapshot.hasData) {
+             final photoList = asyncSnapshot.data!;
+            if (photoList.isEmpty) {
+              return const Center(child: Text("No photos found"));
+            }
+          
+            return GridView.builder(
+              itemCount: photoList.length,
+              padding: EdgeInsets.all(20),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 15,
+                mainAxisSpacing: 15,
+                childAspectRatio: 0.75
+                ), 
+              itemBuilder: (context, index) {
+                final photo = photoList[index];
+                final imageUrl = photo['src']['medium'];
+                return ImageCard(uri: imageUrl);
+              },
+              );
+          }
+          else {
+            return const Center(child: Text("No data found."));
+          }
+        }
+      )
     );
   }
 }
--- lib/widgets/image_card.dart
+++ lib/widgets/image_card.dart
@@ -1,25 +1,17 @@
 import 'package:flutter/material.dart';
 
 class ImageCard extends StatelessWidget {
-  const ImageCard({super.key});
+  final uri;
+  const ImageCard({super.key, required this.uri});
  
   @override
   Widget build(BuildContext context) {
-    const apiKey = String.fromEnvironment("API_KEY");
-    if (apiKey.isEmpty) {
-      throw Exception("API_KEY not found");
-    }
     return ClipRRect(
           borderRadius: BorderRadiusGeometry.all(Radius.circular(20)),
-          child: Column(
-            children: [
-              // Image.network(
-              //   "https://images.pexels.com/photos/34385548/pexels-photo-34385548.jpeg",
-              //   fit: BoxFit.contain,
-              //   ),
-              Text("API_KEY: $apiKey"),
-            ],
-          ),
+          child: Image.network(
+                uri,
+                fit: BoxFit.cover,
+                ),
         );
   }
 }
